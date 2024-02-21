866e6ea4c3dbff2bad75ed18a09b02081a6171e9
Ranvir Singh <sin12756@shernet.sheridancollege.ca>
2024-02-21 12:38:38 -0500

    comit r

---
+++ src/midtermexam_versiona_extensioncode/Ranvir.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author hp
+ */
+public class Ranvir {
+    public static void main(String[] args) {
+    System.out.println("I am a good boy");
+}}
